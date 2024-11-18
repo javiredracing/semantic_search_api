4b2a85ede482b2d44f9c727cb041dd55eaff9405
Javier Fernández <javiredracing@gmail.com>
2024-11-18 14:23:30 +0000

    Upload file refactorized Upload file from url method added

--- app/apis/api_documents/write_docs.py
+++ app/apis/api_documents/write_docs.py
@@ -1,47 +1,68 @@
 from __future__ import annotations
 
-import concurrent
-import time
+import os
 import logging
 
 from datetime import datetime
+
 import requests
 from haystack import Document
+from haystack.document_stores.types import DuplicatePolicy
 from haystack_integrations.document_stores.elasticsearch import ElasticsearchDocumentStore
+from pydantic import FileUrl
 
 from app.apis.api_documents.ProcessText import ProcessText
 from app.core.config import EMBEDDINGS_SERVER, EMBEDDINGS_MODEL, DB_HOST
 
-
-
-def document_manager(files, metadata, index:str):
-    start = time.process_time()
-    with concurrent.futures.ThreadPoolExecutor() as executor:  # optimally defined number of threads
-        for i, file in enumerate(files):
-            nameParsed = file.filename.split(".")
-            if len(nameParsed) > 1:
-                meta = None
-                if metadata and len(metadata) > i:
-                    meta = metadata[i]
-                executor.submit(processFile, file, nameParsed[1], meta, index)
-                # processFile(file,nameParsed[1], meta)
+def download_process_file(url_file:FileUrl, metadata:dict, index:str):
+    try:
+        response = requests.get(url_file)
+        if response.status_code == 200:
+            filename = response.headers.get("Content-Disposition")
+            if filename:
+                filename = filename.split("=")[-1].strip().strip('"')
             else:
-                logging.warning("Error! no valid extension")
+                filename = os.path.basename(url_file.path)
+            content_type = response.headers.get("Content-Type")
+            if len(filename.split(".")) > 0:
+                if not content_type:
+                    content_type = filename.split(".")[1]
+                processFile(file=response.content,filename=filename, content_type=content_type, metadata=metadata, index=index)
+    except requests.RequestException:
+        pass
+    return None
 
-    end = time.process_time()
-    logging.info("Processing time:", end - start)
+def processFile(file:bytes, filename:str, content_type:str, metadata:dict, index:str):
+    nameParsed = filename.split(".")
+    if len(nameParsed) > 1:
+        texts, pages = ProcessText.readFile(file, nameParsed[1])
+        if nameParsed[1] == ProcessText.SRT_FORMAT:
+            docs = generateSRT_doc(filename=filename, srt_text= texts[0], metadata=metadata)
+        else:
+            docs = generate_doc(filename=filename, content_type=content_type, texts=texts, metadata=metadata, pages=pages)
+        save_docs(docs=docs,index=index)
 
+def processPlainText(filename:str, plain_text:str, metadata:dict, index:str):
+    ext = filename.split(".")
+    if len(ext) > 0:
+        if ext[1] == ProcessText.SRT_FORMAT:
+            docs = generateSRT_doc(filename= filename, srt_text=plain_text, metadata=metadata)
+            save_docs(docs=docs,index=index)
+        elif ext[1] == ProcessText.VALID_FORMATS:
+            texts, pages = ProcessText.read_plain_text(plain_text=plain_text)
+            docs = generate_doc(filename=filename, content_type="text/plain", texts=texts, metadata=metadata, pages=pages)
+            save_docs(docs=docs,index=index)
 
-def processFile(file, ext, metadata, index:str):
-    texts, pages = ProcessText.readFile(file.file.read(),ext)
-    if ext == ProcessText.SRT_FORMAT:
-        generateSRT_doc(file.filename, texts[0], metadata, index)
-    else:
-        generate_doc(file, texts, metadata, pages, index)
+def save_docs(docs:list, index:str):
+    if len(docs) > 0:
+        document_store = ElasticsearchDocumentStore(hosts=DB_HOST, index="semantic_search")  # TODO: Replace index
+        document_store.write_documents(documents=docs,policy=DuplicatePolicy.SKIP)
+        document_store.client.close()
 
-
-#plain text srt to dict
-def parseSRT(texts:str) -> []:
+def parseSRT(texts:str) -> list:
+    """
+    Parse plain text srt to dict
+    """
     splitted_srt = texts.strip().split("\n\n")
     splitted_srt = [x for x in splitted_srt if x.strip()] #remove whites
     current_texts = []
@@ -65,14 +86,14 @@ def parseSRT(texts:str) -> []:
     return current_texts
 
 
-def generateSRT_doc(filename:str, srt_text:str, metadata:dict, index:str):
+def generateSRT_doc(filename:str, srt_text:str, metadata:dict) -> list:
     # print(filename,metadata)
     srt_json = parseSRT(srt_text)
     docs = []
 
     current_texts = []
-    for audio_chunk in srt_json:
-        current_texts.append(audio_chunk["content"].replace("\"", "\'"))
+    for srt_chunk in srt_json:
+        current_texts.append(srt_chunk["content"].replace("\"", "\'"))
 
     if len(current_texts) > 0:
         try:
@@ -84,19 +105,15 @@ def generateSRT_doc(filename:str, srt_text:str, metadata:dict, index:str):
                 dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
                 for i, text in enumerate(current_texts):
                     currentMetadata = srt_json[i]["metadata"]
-
                     currentMetadata.update(metadata)
                     currentMetadata.update({"timestamp": dt_string, "page": 1, "name": filename, "file_type": "srt"})
                     docs.append(Document(content=text, meta=currentMetadata, embedding=doc_emb[i]["embedding"]))
         except requests.exceptions.RequestException as e:
             logging.error(e.response.text)
-    if len(docs) > 0:
-        document_store = ElasticsearchDocumentStore(hosts=DB_HOST, index="semantic_search")  # TODO: Replace index
-        document_store.write_documents(docs)
-        document_store.client.close()
 
+    return docs
 
-def generate_doc(file, texts, metadata, pages, index:str):
+def generate_doc(filename:str, content_type:str, texts:list, metadata:dict, pages:list) -> list:
     docs = []
     if len(texts) > 0:
         try:
@@ -107,8 +124,8 @@ def generate_doc(file, texts, metadata, pages, index:str):
                 now = datetime.now()
                 dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
                 for i, text in enumerate(texts):
-                    currentMetadata = {"name": file.filename, "paragraph": i, "timestamp": dt_string,
-                                       "file_type": file.content_type}
+                    currentMetadata = {"name": filename, "paragraph": i, "timestamp": dt_string,
+                                       "file_type": content_type}
                     if metadata:
                         currentMetadata.update(metadata)
                     if len(pages) == len(texts):
@@ -121,7 +138,5 @@ def generate_doc(file, texts, metadata, pages, index:str):
         except requests.exceptions.RequestException as e:
             logging.error(e.response.text)
 
-    if len(docs) > 0:
-        document_store = ElasticsearchDocumentStore(hosts=DB_HOST, index="semantic_search")  # TODO: Replace index
-        document_store.write_documents(docs)
-        document_store.client.close()
+    return docs
+
--- app/routes/views_upload.py
+++ app/routes/views_upload.py
@@ -2,19 +2,20 @@ from __future__ import annotations
 
 import copy
 import json
+import os.path
 from typing import Optional, Dict, Annotated, List
 
 from fastapi import APIRouter, Body, BackgroundTasks, UploadFile, File
-from pydantic import BaseModel, ConfigDict, Field, model_validator
+from pydantic import BaseModel, ConfigDict, Field, model_validator, HttpUrl
 
-from app.apis.api_documents.write_docs import generateSRT_doc, document_manager
+from app.apis.api_documents.write_docs import download_process_file, processFile, processPlainText
 from app.core.auth import decode_access_token
 
 
 class InputParams(BaseModel):
     model_config = ConfigDict(extra='forbid')
     token: str = Field(title="Access token", max_length=175)
-    metadata: Optional[List[dict,]] = None
+    metadata: Optional[dict] = None
 
     @model_validator(mode='before')
     @classmethod
@@ -23,6 +24,8 @@ class InputParams(BaseModel):
             return cls(**json.loads(value))
         return value
 
+class InputParamsURL(InputParams):
+    url_files: list[HttpUrl]
 
 class PlainSRTParams(BaseModel):
     model_config = ConfigDict(extra='forbid')
@@ -37,41 +40,52 @@ class ReturnUpload(BaseModel):
 
 router = APIRouter()
 
-@router.post("/upload/plainSRT/", tags=["documents"])
-def upload_plain_srt(input_values:PlainSRTParams, background_tasks: BackgroundTasks) -> ReturnUpload:
+@router.post("/upload/plainText/", tags=["documents"])
+def upload_plain_text(input_values:PlainSRTParams, background_tasks: BackgroundTasks) -> ReturnUpload:
     """
-    Process .srt files in plain text.
-    - text: Plain srt file
+    Process .srt and .txt files in plain text.
+    - text: Plain text file
     - File name required.
     - Metadata example for each file added: `{"user": ["some", "more"], "category": ["only_one"]}`    OR    empty
-    - Token access required
+    - Access token required
     """
-
     index = decode_access_token(input_values.token)
     print(index)
-
-    background_tasks.add_task(generateSRT_doc, input_values.filename, input_values.text, input_values.metadata, index)
-    return ReturnUpload(message="SRT plain text uploaded correctly, processing...", filenames=input_values.filename)
+    background_tasks.add_task(processPlainText, input_values.filename, input_values.text, input_values.metadata, index)
+    return ReturnUpload(message="Plain text uploaded correctly, processing...", filenames=input_values.filename)
 
 
 @router.post("/upload/", tags=["documents"])
 def upload_documents(files: Annotated[List[UploadFile], File(description="Document files to upload")], background_tasks: BackgroundTasks,
-                     metadata: InputParams = Body(...)) -> ReturnUpload:
+                     params: InputParams = Body(...)) -> ReturnUpload:
     """
     Accepts documents in **.doc, .pdf .xps, .epub, .mobi, .fb2, .cbz, .svg, .txt and .srt** format. It only can be parsed by paragraphs.
-
-    Metadata example for each file added: `{"name": ["some", "more"], "category": ["only_one"]}`    OR    empty
+    - Metadata example for each file added: `{"name": ["some", "more"], "category": ["only_one"]}`    OR    empty
+    - Access token required
     """
     #TODO: Add support for other parsing options.
-    index = decode_access_token(metadata.token)
+    index = decode_access_token(params.token)
     print(index)
-    #
+    filenames= []
     # https://stackoverflow.com/questions/63110848/how-do-i-send-list-of-dictionary-as-body-parameter-together-with-files-in-fastap
     for file in files:
-        if file.content_type.startswith("audio/"):
-            return ReturnUpload(message="Error uploading files! This endpoint only accepts text format files.", filenames=file.filename)
+        if not file.content_type.startswith("audio/"):
+            upload_file = copy.deepcopy(file)
+            f = upload_file.file.read()
+            background_tasks.add_task(processFile, f, file.filename, file.content_type,params.metadata, index)
+            filenames.append(file.filename)
+    return ReturnUpload(message=f"Processing {str(len(filenames))} documents", filenames="".join(filenames))
 
-    background_tasks.add_task(document_manager, copy.deepcopy(files), metadata.metadata, index)
-    return ReturnUpload(message="Processing documents", filenames="".join([file.filename for file in files]))
 
-#TODO upload from url
+@router.post("/upload_url/", tags=["documents"])
+def upload_documents_from_url(params: InputParamsURL, background_tasks: BackgroundTasks) -> ReturnUpload:
+    """
+    Accepts accessible URL files in **.doc, .pdf .xps, .epub, .mobi, .fb2, .cbz, .svg, .txt and .srt** format. It only can be parsed by paragraphs.
+    - Metadata example for each file added: `{"name": ["some", "more"], "category": ["only_one"]}`    OR    empty
+    - Access token required
+    """
+    index = decode_access_token(params.token)
+    print(index)
+    for url_file in params.url_files:
+        background_tasks.add_task(download_process_file, url_file, params.metadata, index)
+    return ReturnUpload(message=f"Processing {str(len(params.url_files))} documents", filenames="".join([os.path.basename(url.path) for url in params.url_files]))
