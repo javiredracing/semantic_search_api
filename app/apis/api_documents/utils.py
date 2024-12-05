7e72f2a3282a755f9805d94618d2c4271354d84d
Javier Fernández <javiredracing@gmail.com>
2024-12-05 14:10:33 +0000

    Code revised

--- app/apis/api_documents/utils.py
+++ app/apis/api_documents/utils.py
@@ -2,7 +2,6 @@ import logging
 
 import requests
 from haystack.components.joiners import DocumentJoiner
-from haystack.document_stores.types import FilterPolicy
 from haystack_integrations.components.retrievers.elasticsearch import ElasticsearchBM25Retriever, \
     ElasticsearchEmbeddingRetriever
 from haystack_integrations.document_stores.elasticsearch import ElasticsearchDocumentStore
